2b644335957a3af3822e82ce61c75aa445c3d60a
Pamela Frieze <pfrieze@gmail.com>
2017-09-22 23:49:34 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -4,12 +4,12 @@ def badge_maker(name)
 end
 
 def batch_badge_creator(attendees)
-attendees = ["Edsger", "Ada", "Charles", "Alan", "Grace", "Linus", "Matz"]
 attendees.map do |attendee|
   "Hello, my name is #{attendee}."
 end
 end
 
 def assign_rooms(speaker, room)
+  
 return "Hello, #{speaker} You'll be assigned to #{room}!"
 end
