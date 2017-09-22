ecd0c8892f7d1f9944196bbe4a0b23e9b74eb265
Pamela Frieze <pfrieze@gmail.com>
2017-09-22 23:52:23 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -11,5 +11,5 @@ end
 
 def assign_rooms(attendees)
 attendees.each_with_index.map do |attendee, index|
-return "Hello, #{speaker} You'll be assigned to #{room}!"
+return "Hello, #{attendee} You'll be assigned to #{index + 1}!"
 end
