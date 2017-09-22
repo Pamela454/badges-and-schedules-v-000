b113c759a026814ccd348fbc76addd7a72790bc2
Pamela Frieze <pfrieze@gmail.com>
2017-09-22 23:30:45 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -4,12 +4,6 @@ def badge_maker(name)
 end
 
 def batch_badge_creator(attendees)
-badges = ["Hello, my name is Edsger.",
-"Hello, my name is Ada.",
-"Hello, my name is Charles.",
-"Hello, my name is Alan.",
-"Hello, my name is Grace.",
-"Hello, my name is Linus.",
-"Hello, my name is Matz."]
-return badges
+attendees = ["Edsger", "Ada", "Charles", "Alan", "Grace, "Linus", "Matz"]
+
 end
